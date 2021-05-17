e93afa95f563d684298f7a1e78ddc10e778fe037
Nexxer <yairopb@yandex.ru>
2021-05-17 20:02:38 +0300

    добавлен билд добавления фото в сборку

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 'use strict';
 
+const gulp = require('gulp');
+
 module.exports = require('bootstrap-gulp');
+
+gulp.task('build-images-dev', function() {
+  return gulp.src(['source/img/**/*.{gif,jpg,png,svg}'])
+  .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('source/img/**/*.{gif,jpg,png,svg}', ['build-images-dev']);
+});
